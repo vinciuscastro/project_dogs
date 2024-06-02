00b672d87864bd14ac69d75c0555144454afe120
vinciuscastro <vinisilvacastro71@gmail.com>
2024-06-02 01:58:47 -0300

    Header v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+import Home from './Components/Home'
+import Login from './Components/Login/Login'
+
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 //import Dog from './img/dog.svg?react';
+
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
   )
 }
 
